92fdb1a00f32489c45de0709a2f71f7a4491fa2d
allefree <allexfree@yandex.ru>
2018-11-06 22:44:10 +0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,30 @@
+'use strict';
+
 var gulp = require('gulp'),
-    scss = require('gulp-scss'),
-    browserSync = require('browser-sync');
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('serve', ['taskScss', 'watch'], function() {
+  browserSync.init({
+    server: './'
+  })
+});
+
+gulp.task('taskScss', function() {
+  return gulp.src('src/scss/**/*.scss')
+  .pipe(sass({
+    includePaths: require('node-normalize-scss').includePaths
+  }).on('error', notify.onError()))
+  .pipe(rename('style.css'))
+  .pipe(gulp.dest('css'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/scss/**/*.scss', ['taskScss']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
